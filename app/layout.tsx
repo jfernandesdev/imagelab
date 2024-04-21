f81feef9768cc0fca19d00a6479a7aed3c890c14
Jeferson Fernandes <jfernandes.dev@gmail.com>
2024-04-21 20:27:00 -0300

    FEAT: clerk authentication configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { ptBR } from "@clerk/localizations";
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
-import { cn } from "@/lib/utils";
 
 const poppins = Poppins({ 
   subsets: ["latin"],
@@ -20,10 +23,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
-      <body className={cn("font-poppins antialiased", poppins.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider 
+      localization={ptBR} 
+      appearance={{
+        variables: {
+          colorPrimary: '#9747FF'
+        }
+      }}>
+      <html lang="pt-br">
+        <body className={cn("font-poppins antialiased", poppins.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
