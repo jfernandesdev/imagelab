65a9edaf3b3e671f83e0948707ccdb87d016bd24
Jeferson Fernandes <jfernandes.dev@gmail.com>
2024-05-01 14:59:07 -0300

    FEAT: server actions and webhook from user create, update and delete

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,156 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+/* eslint-disable prefer-const */
+/* eslint-disable no-prototype-builtins */
+import { type ClassValue, clsx } from "clsx";
+import qs from "qs";
+import { twMerge } from "tailwind-merge";
 
+import { aspectRatioOptions } from "@/constants";
+
+export type AspectRatioKey = keyof typeof aspectRatioOptions;
+
+// Função para combinar classes usando Tailwind CSS
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// Manipulador de erros
+export const handleError = (error: unknown) => {
+  if (error instanceof Error) {
+    console.error(error.message);
+    throw new Error(`Error: ${error.message}`);
+  } else if (typeof error === "string") {
+    console.error(error);
+    throw new Error(`Error: ${error}`);
+  } else {
+    console.error(error);
+    throw new Error(`Unknown error: ${JSON.stringify(error)}`);
+  }
+};
+
+// Loader para quando a imagem está sendo transformada
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#7986AC" offset="20%" />
+      <stop stop-color="#68769e" offset="50%" />
+      <stop stop-color="#7986AC" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#7986AC" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+// Converte o SVG para uma URL base64
+const toBase64 = (str: string) =>
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
+
+export const dataUrl = `data:image/svg+xml;base64,${toBase64(
+  shimmer(1000, 1000)
+)}`;
+
+// Formulário de consulta via url
+export const formUrlQuery = ({
+  searchParams,
+  key,
+  value,
+}: FormUrlQueryParams) => {
+  const params = { ...qs.parse(searchParams.toString()), [key]: value };
+
+  return `${window.location.pathname}?${qs.stringify(params, {
+    skipNulls: true,
+  })}`;
+};
+
+// Remove chave da consulta
+export function removeKeysFromQuery({
+  searchParams,
+  keysToRemove,
+}: RemoveUrlQueryParams) {
+  const currentUrl = qs.parse(searchParams);
+
+  keysToRemove.forEach((key) => {
+    delete currentUrl[key];
+  });
+
+  // Remove valores nulos ou indefinidos
+  Object.keys(currentUrl).forEach(
+    (key) => currentUrl[key] == null && delete currentUrl[key]
+  );
+
+  return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
+}
+
+// Debounce
+export const debounce = (func: (...args: any[]) => void, delay: number) => {
+  let timeoutId: NodeJS.Timeout | null;
+  return (...args: any[]) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(null, args), delay);
+  };
+};
+
+// Saber tamanho da imagem
+export const getImageSize = (
+  type: string,
+  image: any,
+  dimension: "width" | "height"
+): number => {
+  if (type === "fill") {
+    return (
+      aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
+      1000
+    );
+  }
+  return image?.[dimension] || 1000;
+};
+
+// Download da imagemm
+export const download = (url: string, filename: string) => {
+  if (!url) {
+    throw new Error("Resource URL not provided! You need to provide one");
+  }
+
+  fetch(url)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const blobURL = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = blobURL;
+
+      if (filename && filename.length)
+        a.download = `${filename.replace(" ", "_")}.png`;
+      document.body.appendChild(a);
+      a.click();
+    })
+    .catch((error) => console.log({ error }));
+};
+
+// Unir objetos
+export const deepMergeObjects = (obj1: any, obj2: any) => {
+  if (obj2 === null || obj2 === undefined) {
+    return obj1;
+  }
+
+  let output = { ...obj2 };
+
+  for (let key in obj1) {
+    if (obj1.hasOwnProperty(key)) {
+      if (
+        obj1[key] &&
+        typeof obj1[key] === "object" &&
+        obj2[key] &&
+        typeof obj2[key] === "object"
+      ) {
+        output[key] = deepMergeObjects(obj1[key], obj2[key]);
+      } else {
+        output[key] = obj1[key];
+      }
+    }
+  }
+
+  return output;
+};
